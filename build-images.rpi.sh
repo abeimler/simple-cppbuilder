d0bb24c67798a45f0663324829be06fbde16a12d
abeimler <alex-beimler@web.de>
2021-08-13 10:05:58 +0200

    build rpi1 image

--- build-images.rpi.sh
+++ build-images.rpi.sh
@@ -22,7 +22,7 @@ docker build --force-rm=true -f ./Dockerfile.rpi \
     --build-arg vcpkg_target_triplet="arm-linux" \
     --build-arg cross_triple="armv6-unknown-linux-gnueabihf" \
     --build-arg cmake_toolchain_file="/home/cmake/arm-rpi-toolchain.cmake" \
-    -t abeimler/simple-cppbuilder:rpi .
+    -t abeimler/simple-cppbuilder:rpi1 .
 
 docker build --force-rm=true -f ./Dockerfile.rpi \
     --build-arg vcpkg_target_triplet="arm64-linux" \
