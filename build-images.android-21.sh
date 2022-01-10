1a944ac23ffa700230e2ae49fedaa76b9adaca4a
abeimler <alex-beimler@web.de>
2022-01-10 19:51:47 +0100

    add ubuntu base image

--- build-images.android-21.sh
+++ build-images.android-21.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-docker build --force-rm=true -f ./Dockerfile.android \
+docker build --force-rm=true -f ./Dockerfile.android-21 \
     --build-arg vcpkg_target_triplet="arm-android" \
     --build-arg cmake="android-armv7a-eabi-cmake" \
     --build-arg android_abi="armeabi-v7a" \
-    -t abeimler/simple-cppbuilder:arm-android .
+    -t abeimler/simple-cppbuilder:arm-android-21 .
 
-docker build --force-rm=true -f ./Dockerfile.android \
+docker build --force-rm=true -f ./Dockerfile.android-21 \
     --build-arg vcpkg_target_triplet="arm64-android" \
     --build-arg cmake="android-aarch64-cmake" \
     --build-arg android_abi="arm64-v8a" \
-    -t abeimler/simple-cppbuilder:arm64-android .
+    -t abeimler/simple-cppbuilder:arm64-android21 .
 
-docker build --force-rm=true -f ./Dockerfile.android \
+docker build --force-rm=true -f ./Dockerfile.android-21 \
     --build-arg vcpkg_target_triplet="x86-android" \
     --build-arg cmake="android-x86-cmake" \
     --build-arg android_abi="x86" \
-    -t abeimler/simple-cppbuilder:x86-android .
+    -t abeimler/simple-cppbuilder:x86-android-21 .
 
-docker build --force-rm=true -f ./Dockerfile.android \
+docker build --force-rm=true -f ./Dockerfile.android-21 \
     --build-arg vcpkg_target_triplet="x64-android" \
     --build-arg cmake="android-x86-64-cmake" \
     --build-arg android_abi="x86_64" \
-    -t abeimler/simple-cppbuilder:x64-android .
+    -t abeimler/simple-cppbuilder:x64-android-21 .
--- build-images.arm.sh
+++ build-images.arm.sh
@@ -23,21 +23,3 @@ docker build --force-rm=true -f ./Dockerfile.cross \
 #    --build-arg cmake="cmake" \
 #    --build-arg cmake_toolchain_file="/home/cmake/arm-toolchain.cmake" \
 #    -t abeimler/simple-cppbuilder:arm-linux-gnueabihf .
-
-docker build --force-rm=true -f ./Dockerfile.crosstool-ng \
-    --build-arg vcpkg_target_triplet="arm-linux" \
-    --build-arg cross_triple="arm-unknown-linux-gnueabi" \
-    --build-arg cmake_toolchain_file="/home/cmake/arm-toolchain.cmake" \
-    -t abeimler/simple-cppbuilder:arm-unknown-linux-gnueabi .
-    
-#docker build --force-rm=true -f ./Dockerfile.crosstool-ng \
-#    --build-arg vcpkg_target_triplet="avr" \
-#    --build-arg cross_triple="avr" \
-#    --build-arg cmake_toolchain_file="/home/cmake/avr-toolchain.cmake" \
-#    -t abeimler/simple-cppbuilder:avr .
-
-#docker build --force-rm=true -f ./Dockerfile.crosstool-ng \
-#    --build-arg vcpkg_target_triplet="ppc64le-linux" \
-#    --build-arg cross_triple="powerpc64le-unknown-linux-gnu" \
-#    --build-arg cmake_toolchain_file="/home/cmake/ppc64le-toolchain.cmake" \
-#    -t abeimler/simple-cppbuilder:powerpc64le-unknown-linux-gnu .
